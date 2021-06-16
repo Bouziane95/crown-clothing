dfa7a2f02cb50b5f57dab55e79a671d4716e94dd
BEY <bouziane.bey@capgemini.com>
2021-06-16 16:27:14 +0200

    updated shop data with new data and group items with the quantity

--- src/components/collection-preview/collection-preview.component.js
+++ src/components/collection-preview/collection-preview.component.js
@@ -10,8 +10,8 @@ const CollectionPreview = ({title, items}) => {
             {
                 items
                 .filter((item, index) => index < 4)
-                .map((item) => {
-                    return <CollectionItem key={item.id} item= {item}/>
+                .map(item => {
+                    <CollectionItem key={item.id} item= {item}/>
                 })
             }
             </div>
